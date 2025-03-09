01359739ca56bb32b9a58c5843a8c902c0f56c15
vonhatphuongahihi <vonhatphuongahihi@gmail.com>
2025-03-09 16:39:27 +0700

    scanner

--- src/PhuongHocJava/HelloWorld.java
+++ src/PhuongHocJava/HelloWorld.java
@@ -2,7 +2,9 @@ package PhuongHocJava;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		int a = 6;
+		int b = 9;
+		int c = a + b;
+		System.out.println("Sum: " + c);
 	}
 }
---
+++ src/PhuongHocJava/LearnScanner.java
@@ -0,0 +1,15 @@
+package PhuongHocJava;
+
+import java.util.Scanner;
+
+public class LearnScanner {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
+		System.out.println("My name is " + name + ", age = " + age);
+		scanner.close();
+	}
+}
